b001e7ffb5cb525c723d31521edd6b382b7eacb5
kenshi39847 <kenshi.t.blue@gmail.com>
2024-02-04 20:58:25 +0900

    コントローラへの追記

--- src/main/java/com/example/demo/controller/ThymeleafController.java
+++ src/main/java/com/example/demo/controller/ThymeleafController.java
@@ -1,6 +1,7 @@
 package com.example.demo.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
@@ -8,6 +9,8 @@ public class ThymeleafController {
 	
 	@GetMapping("show")
 	public String showView(Model model) {
+		//Modelにデータを格納する
+		model.addAttribute("name", "太郎");
 		//戻り値はビュー名を返す
 		return "useThymeleaf";
 	}
